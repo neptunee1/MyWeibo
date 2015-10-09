e6c3cf9fd567f7f4a6c52c4743b82958bc390cd0
CKK <479459938@qq.com>
2015-10-09 15:10:51 +0800

    修改userinfo

--- MyWeibo/MyWeibo/ViewController.m
+++ MyWeibo/MyWeibo/ViewController.m
@@ -25,10 +25,13 @@
     WBAuthorizeRequest *request = [WBAuthorizeRequest request];
     request.redirectURI = kRedirectURI;
     request.scope = @"all";
-    request.userInfo = @{@"SSO_From": @"ViewController",
-                         @"Other_Info_1": [NSNumber numberWithInt:123],
-                         @"Other_Info_2": @[@"obj1", @"obj2"],
-                         @"Other_Info_3": @{@"key1": @"obj1", @"key2": @"obj2"}};
+    
+    //可以不设置
+//    request.userInfo = @{@"SSO_From": @"ViewController",
+//                         @"Other_Info_1": [NSNumber numberWithInt:123],
+//                         @"Other_Info_2": @[@"obj1", @"obj2"],
+//                         @"Other_Info_3": @{@"key1": @"obj1", @"key2": @"obj2"}};
+    
     [WeiboSDK sendRequest:request];
 
     
